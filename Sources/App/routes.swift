56cab2b4857a776caddf25b8be639ed913d2333f
He Wu(Heva) <hewu9375@gmail.com>
2021-06-20 19:09:20 +0900

    fix leaf init

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,8 +3,6 @@ import Vapor
 func routes(_ app: Application) throws {
     app.get { req in
         return req.view.render("index", ["title": "Hello Vapor!"])
-    }app.get { req in
-        return "It works!"
     }
 
     app.get("hello") { req -> String in
